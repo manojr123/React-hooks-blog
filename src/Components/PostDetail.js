65ef6a51196a5517cd23820041cfec0df965c70b
manojr123 <manojraghavan2008@yahoo.co.in>
2023-04-14 14:34:29 +0530

    Added Post Detail code

--- src/Components/PostDetail.js
+++ src/Components/PostDetail.js
@@ -1,6 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import firebase from "firebase/app";
+import "firebase/firestore";
+
 function PostDetail() {
-    return <div className="">PostDetail</div>;
-  }
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
   
-  export default PostDetail;
-  
+  const firestore = firebase.firestore();
+
+  useEffect(() => {
+    firestore
+      .collection('posts')
+      .doc(postId)
+      .get()
+      .then((snapshot) => {
+        console.log('snapshot', snapshot.data());
+        setPost(snapshot.data());
+      });
+  }, []);
+
+  return (
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
+}
+
+export default PostDetail;
